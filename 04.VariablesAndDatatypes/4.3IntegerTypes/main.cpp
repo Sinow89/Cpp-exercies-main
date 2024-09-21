a00625d0df34a3f5713db62c012345f4f5dd2198
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2024-09-21 11:11:37 +0200

    Update main.cpp

--- 04.VariablesAndDatatypes/4.3IntegerTypes/main.cpp
+++ 04.VariablesAndDatatypes/4.3IntegerTypes/main.cpp
@@ -6,17 +6,20 @@ int main(){
     
     //Variable may contain random garbage value . WARNING
     int elephant_count;
-    
     int lion_count{};//Initializes to zero
-    
     int dog_count {10}; //Initializes to 10
-    
     int cat_count {15}; //Initializes to 15
-    
     //Can use expression as initializer
     int domesticated_animals { dog_count + cat_count };
 
-    std::cout << "Dogs and cats : " << domesticated_animals << std::endl;
+   // std::cout << "Dogs and cats : " << domesticated_animals << std::endl;
+   // int new_number {doesnt_exist};
+   //double narrowing_conversion {2.9};
+    
+    std::cout << "Elephant count : " << elephant_count << std::endl;
+    std::cout << "Lion count : " << lion_count << std::endl;
+    std::cout << "Dog count : " << dog_count << std::endl;
+    std::cout << "Cat count :" << cat_count << std::endl;
 
     return 0;
 
