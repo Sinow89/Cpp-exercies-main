80e140c588049a36e819897a05d82d3926265a5f
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2024-09-21 11:00:49 +0200

    Update main.cpp

--- 04.VariablesAndDatatypes/4.3IntegerTypes/main.cpp
+++ 04.VariablesAndDatatypes/4.3IntegerTypes/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
-consteval int get_value(){
-    return 3;
-}
-
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+
+    //Braced initializers
+    
+    //Variable may contain random garbage value . WARNING
+    int elephant_count;
+    
+    int lion_count{};//Initializes to zero
+    
+    int dog_count {10}; //Initializes to 10
+    
+    int cat_count {15}; //Initializes to 15
+    
+    //Can use expression as initializer
+    int domesticated_animals { dog_count + cat_count };
+
+    std::cout << "Dogs and cats : " << domesticated_animals << std::endl;
+
     return 0;
+
 }
