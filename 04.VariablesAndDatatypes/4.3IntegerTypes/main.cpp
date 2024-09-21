1888cc5498244d763f2a15f0dfb1121c9f5b1483
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2024-09-21 11:21:59 +0200

    Update main.cpp

--- 04.VariablesAndDatatypes/4.3IntegerTypes/main.cpp
+++ 04.VariablesAndDatatypes/4.3IntegerTypes/main.cpp
@@ -3,7 +3,7 @@
 int main(){
 
     //Braced initializers
-    
+    /*
     //Variable may contain random garbage value . WARNING
     int elephant_count;
     int lion_count{};//Initializes to zero
@@ -20,6 +20,41 @@ int main(){
     std::cout << "Lion count : " << lion_count << std::endl;
     std::cout << "Dog count : " << dog_count << std::endl;
     std::cout << "Cat count :" << cat_count << std::endl;
+    std::cout << "Dog and cat count :" << domesticated_animals << std::endl;
+    
+    */
+    
+    
+    //Functional Initialization
+   
+    // int apple_count(5);
+    // int orange_count(10);
+    // int fruit_count (apple_count + orange_count);
+    // //int bad_initialization ( doesnt_exist3 + doesnt_exist4 );
+
+    // //Information lost. less safe than braced initializers
+    // int narrowing_conversion_functional (2.9);
+    
+    
+    // std::cout << "Apple count : " << apple_count << std::endl;
+    // std::cout << "Orange count : " << orange_count << std::endl;
+    // std::cout << "Fruit count : " << fruit_count << std::endl;
+    // std::cout << "Narrowing conversion : " << narrowing_conversion_functional << std::endl;//Will loose info
+    
+    
+        
+    int bike_count = 2;
+    int truck_count = 7;
+    int vehicle_count = bike_count + truck_count;
+    int narrowing_conversion_assignment = 2.9;
+
+    std::cout << "Bike count : " << bike_count << std::endl;
+    std::cout << "Truck count : " << truck_count << std::endl;
+    std::cout << "Vehicle count : " << vehicle_count << std::endl;
+    std::cout << "Narrowing conversion : " << narrowing_conversion_assignment << std::endl;
+    
+    std::cout << "sizeof int : " << sizeof(int) << std::endl;
+    std::cout << "sizeof truck_count : " << sizeof(truck_count) << std::endl;
 
     return 0;
 
