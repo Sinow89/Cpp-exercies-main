fb1daa8ef3a6e411a8a991411a85b0987e35ff9e
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-24 18:42:10 +0200

    New files

--- 04.VariablesAndDatatypes/4.6Booleans/main.cpp
+++ 04.VariablesAndDatatypes/4.6Booleans/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
-
 int main(){
-
+    
     bool red_light {false};
     bool green_light{true};
     
@@ -20,14 +19,15 @@ int main(){
 
     //sizeof()
     std::cout << "sizeof(bool) : " << sizeof(bool) << std::endl;
-
-
+    
+    
     //Printing out a bool
     //1 -->> true
     //0 -->> false
     std::cout << std::endl;
     std::cout << "red_light : " << red_light << std::endl;
     std::cout << "green_light : " << green_light << std::endl;
+
 
     std::cout << std::boolalpha;
     std::cout << "red_light : " << red_light << std::endl;
