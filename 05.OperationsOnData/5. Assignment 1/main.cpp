db35d166e1f1e3bf3ac0b1b22df60b7249bb5282
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2024-11-09 16:02:10 +0100

    5. Assignment

---
+++ 05.OperationsOnData/5. Assignment 1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+
+int main(){
+
+   
+   int celcius = 0;
+
+std::cout << "Please enter a celcius number to convert to Fahrenheit" << std::endl;
+std::cin >> celcius;
+std::cout << celcius << std::endl; 
+
+    return 0;
+}
