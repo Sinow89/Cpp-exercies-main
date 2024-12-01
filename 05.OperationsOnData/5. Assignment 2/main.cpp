8aa49133cc250f487a8b2d3241e0f4bbe37197db
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2024-12-01 22:23:44 +0100

    Next assignment

---
+++ 05.OperationsOnData/5. Assignment 2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// #include <iomanip>
+
+int main(){
+   
+   float length;
+   float width;
+   float height;
+
+std::cout << "Welcome to the box calculator. Please type in length, width and height information :" << std::endl;
+std:: cout << "Length : ", std::cin >> length;
+std:: cout << "Width : ", std::cin >> width;
+std:: cout << "Hieght : ", std::cin >> height;
+float base = length * width;
+std::cout << "The base area is : " << base  << std::endl; 
+std::cout << "The volume is : " << (base*height)  << std::endl; 
+
+    return 0;
+}
