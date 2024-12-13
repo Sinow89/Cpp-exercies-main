6cc1a64be9f746630d57b2241405e6bd7937ce5b
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-12-13 11:58:22 +0100

    Update.

--- 06.LiteralsAndConstants/6.2Literals/main.cpp
+++ 06.LiteralsAndConstants/6.2Literals/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
+int main()
+{
 
-int main(){
-
-   
     //Literal types : u and l combinations for unsigned and long.
     unsigned char unsigned_char {53u};// 555U would fail because of narrowing
     
@@ -37,21 +36,18 @@ int main(){
     unsigned int prize  {1'500'00'0u};
     std::cout << "The prize is : " << prize << std::endl;
 
-    std::cout << " signed_long_long_int : " << signed_long_long_int << std::endl;
-  
+    std::cout << "signed_long_long_int" << signed_long_long_int << std::endl;
 
-    //Narrowing errors
-    //Possible narrowing errors are cought by the braced initializer method.
-    //Assignment and functional don't catch that.
-    //unsigned char distance {555u}; //Error [0~255]
-    //unsigned int game_score {-20}; //Error
+    // unsigned char distance {555u}; //Error [0~255]
+    // unsigned int game_score {-20}; //Error
 
-    //std::cout << "game_score : " << game_score << std::endl;
+    // std::cout << "game_score" << game_score << std::endl;
 
-    //With number systems - Hex : prefix with 0x
-    unsigned int hex_number{ 0x22BU}; // Dec 555
+
+        //With number systems - Hex : prefix with 0x
+    unsigned int hex_number{0x22BU}; // Dec 555
     int hex_number2 {0x400};// Dec 1024
-    std::cout << std::hex <<  "The hex number is : " << hex_number << std::endl;
+    std::cout <<  "The hex number is : " << hex_number << std::endl;
     std::cout << std::dec <<  "The hex number2 is : " << hex_number2 << std::endl;
     
     //Representing colors with hex
@@ -59,7 +55,7 @@ int main(){
     std::cout << "Black color is : " << std::dec << black_color << std::endl;
 
 
-    //Octal literals : prefix with 0
+        //Octal literals : prefix with 0
     int octal_number {0777u}; // 511 Dec
     std::cout << "The octal number is : " << octal_number << std::endl;
     //!!BE CAREFUL NOT TO PREFIX YOUR INTEGERS WITH 0 IF YOU MEAN DEC
@@ -71,8 +67,7 @@ int main(){
     unsigned int binary_literal {0b11111111u};// 255 dec
     std::cout << "The binary literal is : " << binary_literal << std::endl;
 
-    // Other literals. This is just an example and we will learn
-    // more about  strings as we progress in the course.
+
     char char_literal {'c'};
     int number_literal {15};
     float fractional_literal {1.5f};
@@ -82,6 +77,6 @@ int main(){
     std::cout << "The number literal is : " << number_literal << std::endl;
     std::cout << "The fractional literal is : " << fractional_literal << std::endl;
     std::cout << "The string literal is : " << string_literal << std::endl;
-   
+
     return 0;
 }
