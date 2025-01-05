98c34583821c4d2426dbba936970044b9aea6a80
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-01-05 17:21:20 +0100

    8.2

--- 08.BitwiseOperators/8.2PrintingIntegersInBinary/main.cpp
+++ 08.BitwiseOperators/8.2PrintingIntegersInBinary/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <bitset>
 
-
 int main(){
 
-    unsigned short int data {15};
+    unsigned short int data {0b01111111};
 
     std::cout << "data (dec) : " <<std::showbase <<  std::dec << data << std::endl;
     std::cout << "data (oct) : " <<std::showbase <<  std::oct << data << std::endl;
     std::cout << "data (hex) : " <<std::showbase <<  std::hex << data << std::endl;
-    std::cout << "data (bin) : " << std::bitset<16>(data) << std::endl;
+    std::cout << "data (bin) : " << std::bitset<8>(data) << std::endl;
    
     return 0;
 }
