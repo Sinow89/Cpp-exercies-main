ea77cc3b6f2bdb810d949bc4ab955fff2dfaf866
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-01-05 14:28:54 +0100

    Added

--- 07.ConversionsOverflowAndUnderflow/7.2ImplicitDataConversions/main.cpp
+++ 07.ConversionsOverflowAndUnderflow/7.2ImplicitDataConversions/main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 
-
 int main(){
 
-	//		. The compiler applies implicit conversions
-	// 			when types are different in 
-	//			an expression
-	//		. Conversions are always done from the smallest
-	//			to the largest type in this case int is 
-	//			transformed to double before the expression
-	//			is evaluated.Unless we are doing an assignment
-	
 	double price { 45.6 };
     int units {10};
     
@@ -18,17 +9,12 @@ int main(){
 	
 	std::cout << "Total price : " << total_price << std::endl;
 	std::cout << "sizeof total_price : " << sizeof(total_price) << std::endl;
-
-
-	//Implicit conversions in assignments
-	// The assignment operation is going to cause an implicit
-	// narrowing conversion , y is converted to int before assignment
-	int x;
+   
+   	int x;
     double y {45.44};
     x = y; // double to int
 	std::cout << "The value of x is : " << x << std::endl; // 45
 	std::cout << "sizeof x : " << sizeof(x) << std::endl;// 4
-	
-   
+
     return 0;
 }
