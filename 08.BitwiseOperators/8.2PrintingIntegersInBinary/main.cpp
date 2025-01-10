9e0d9b34884bf9580520b01a3a3ce1117fb3a8bb
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-01-10 14:44:49 +0100

    Update main.cpp

--- 08.BitwiseOperators/8.2PrintingIntegersInBinary/main.cpp
+++ 08.BitwiseOperators/8.2PrintingIntegersInBinary/main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 
-    unsigned short int data {0b01111111};
+    unsigned short int data {0b10000000};
 
     std::cout << "data (dec) : " <<std::showbase <<  std::dec << data << std::endl;
     std::cout << "data (oct) : " <<std::showbase <<  std::oct << data << std::endl;
