181cb4415abcff8c1cda7bb16b2081842a1c3bfa
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-03-06 14:55:16 +0100

    Next less

--- 10.FlowControl/10.5ShortCircuitEvaluation/main.cpp
+++ 10.FlowControl/10.5ShortCircuitEvaluation/main.cpp
@@ -2,6 +2,28 @@
 
 int main(){
 
+    bool a{ true };
+	bool b{ true };
+	bool c{ true };
+	bool d{ false };
+
+	bool p{ false };
+	bool q{ false };
+	bool r{ false };
+	bool m{ true };
+
+	
+	//AND : If one of the operands is 0, the result is 0
+	std::cout << std::endl;
+	std::cout << "AND short circuit" << std::endl;
+	bool result = a && b && c && d;
+	std::cout << "result : " << std::boolalpha << result << std::endl;
+
+	//OR : If one of the operands is 1, the result is 1.
+	std::cout << std::endl;
+	std::cout << "OR short circuit" << std::endl;
+	result = p || q || r || m;
+	std::cout << "result : " << std::boolalpha << result << std::endl;
 
     return 0;
 }
