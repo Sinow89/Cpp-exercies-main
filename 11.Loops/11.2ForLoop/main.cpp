694c10781c1f4828a0de04c95863132df846b323
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-05-23 15:09:20 +0200

    Ranged for loop

--- 11.Loops/11.2ForLoop/main.cpp
+++ 11.Loops/11.2ForLoop/main.cpp
@@ -19,10 +19,21 @@ void print_uppercase(){
         }
 }
 
+
+void print_value(){
+            for(auto value : {1,2,3,4,5,6,7,8,9,10}){
+                
+            std::cout << "value :" << value << std::endl;
+
+        }
+}
+
 int main(){
 
     std::cout << std::endl;
     print_uppercase();
+    std::cout << std::endl;
+    print_value();
 
     return 0;
 }
